861268bfacadb1e8f065f2172ca1415fd28b116b
AndrewRichards-Code <andrew@simul.co>
2019-02-12 15:36:28 +0000

    Minor update for StaticUpdateWaterProbeValues();

--- Assets/Simul/TrueSkyCamera.cs
+++ Assets/Simul/TrueSkyCamera.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using UnityEngine.Rendering;
 using System.Collections.Generic;
 
-namespace simul 
+namespace simul
 {
 	[ExecuteInEditMode]
 	public class TrueSkyCamera : TrueSkyCameraBase
