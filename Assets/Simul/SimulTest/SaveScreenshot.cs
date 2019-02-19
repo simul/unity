f5af033426fe13911b878133d8bab07b006fa7b4
Simul Build <contact@simul.co>
2019-02-19 14:18:07 +0000

    Option to save screen as different formats.

--- Assets/Simul/SimulTest/SaveScreenshot.cs
+++ Assets/Simul/SimulTest/SaveScreenshot.cs
@@ -7,35 +7,40 @@ public class SaveScreenshot : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
+	{
+		string[] args = System.Environment.GetCommandLineArgs();
+		foreach (string a in args)
+		{
+			try
+			{
+				string[] parts = a.Split('=');
+				if (parts.Length == 2)
+				{
+					if (parts[0].CompareTo("-screenshotfile") == 0)
+						filename = parts[1];
+					if (parts[0].CompareTo("-wait") == 0)
+						waitCount = System.Int32.Parse(parts[1]);
+				}
+			}
+			catch (System.Exception)
+			{
+
+			}
+		}
+		UnityEngine.Debug.Log("screenshotfile " + filename);
+		UnityEngine.Debug.Log("waitCount " + waitCount);
 	}
 	bool got_screenshot = false;
+	int waitCount = 25;
 	string filename = "screenshot.png";
 	private void Update()
 	{
 		//Wait for 25 frames
-		if (UnityEngine.Time.renderedFrameCount < 25)
+		if (UnityEngine.Time.renderedFrameCount < waitCount)
 			return;
 
         if (!got_screenshot)
         {
-            string[] args = System.Environment.GetCommandLineArgs();
-            foreach (string a in args)
-            {
-                try
-                {
-                    string[] parts = a.Split('=');
-                    if (parts.Length == 2)
-                    {
-                        if (parts[0].CompareTo("-screenshotfile") == 0)
-                            filename = parts[1];
-                    }
-                }
-                catch (System.Exception)
-                {
-
-                }
-            }
             try
             {
                 string fullPath = filename;
@@ -44,11 +49,19 @@ public class SaveScreenshot : MonoBehaviour
 				fullPath = fullPath.Replace("\\", "/");
 				string path = fullPath.Substring(0, fullPath.LastIndexOf('/'));
 				string name = fullPath.Substring(path.Length + 1);
+				UnityEngine.Debug.Log("Creating directory "+ path);
 				System.IO.Directory.CreateDirectory(path);
 				Texture2D texture2d = ScreenCapture.CaptureScreenshotAsTexture(1);
-                // Encode texture into PNG
-                byte[] bytes = texture2d.EncodeToPNG();
-                Object.Destroy(texture2d);
+				// Encode texture into PNG
+				byte[] bytes;
+				if(filename.Contains(".png"))
+					bytes = texture2d.EncodeToPNG();
+				else if (filename.Contains(".exr"))
+					bytes = texture2d.EncodeToEXR();
+				else if (filename.Contains(".tga"))
+					bytes = texture2d.EncodeToTGA();
+				else
+					bytes= texture2d.EncodeToJPG();
                 // For testing purposes, also write to a file in the project folder
                 File.WriteAllBytes(fullPath, bytes);
 				got_screenshot = true;
