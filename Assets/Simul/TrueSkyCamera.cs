9c165495e3b9fff55a9695ccc20daf511b74e0b0
AndrewRichards-Code <andrew@simul.co>
2019-04-05 14:14:08 +0100

    TrueSkyCameraBase updated to render TrueSky correctly onto the cubemap based on the TrueSky Master object's transform parameters (Removed parameter inversion issue). Re-enabled post_translucent_buf to display rain.

--- Assets/Simul/TrueSkyCamera.cs
+++ Assets/Simul/TrueSkyCamera.cs
@@ -265,7 +265,7 @@ namespace simul
 			}
 			Marshal.StructureToPtr(unityViewStruct, unityViewStructPtr, true);
 			mainCommandBuffer.IssuePluginEventAndData(UnityGetRenderEventFuncWithData(),TRUESKY_EVENT_ID + cbuf_view_id, unityViewStructPtr);
-			//post_translucent_buf.IssuePluginEventAndData(UnityGetPostTranslucentFuncWithData(), TRUESKY_EVENT_ID + cbuf_view_id, unityViewStructPtr);
+			post_translucent_buf.IssuePluginEventAndData(UnityGetPostTranslucentFuncWithData(), TRUESKY_EVENT_ID + cbuf_view_id, unityViewStructPtr);
 			overlay_buf.IssuePluginEventAndData(UnityGetOverlayFuncWithData(), TRUESKY_EVENT_ID + cbuf_view_id, unityViewStructPtr);
 
 		}
