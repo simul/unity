60dc1841d0dde1f5037bf51956a08e195667ed20
Andrew Richards <andyjrichards93@gmail.com>
2021-03-09 11:13:27 +0000

    Updated resource states for D3D12. Whitespaces changes.

--- Assets/Simul/TrueSkyCameraBase.cs
+++ Assets/Simul/TrueSkyCameraBase.cs
@@ -40,12 +40,34 @@ namespace simul
 			return TRUESKY_EVENT_ID;
 		}
 
-		public enum ResourceState : uint
-		{
-			Unknown = 0,
-			RenderTarget = 4,
-			DepthWrite=16,
-			GenericRead = 0xac3
+        public enum ResourceState : uint
+        {
+			Common = 0,
+			VertexAndConstantBuffer = 0x1,
+			IndexBuffer = 0x2,
+			RenderTarget = 0x4,
+			UnorderedAccess = 0x8,
+			DepthWrite = 0x10,
+			DepthRead = 0x20,
+			NonPixelShaderResource = 0x40,
+			PixelShaderResource = 0x80,
+			StreamOut = 0x100,
+			IndirectArgument = 0x200,
+			CopyDest = 0x400,
+			CopySource = 0x800,
+			ResolveDest = 0x1000,
+			ResolveSource = 0x2000,
+			RaytracingAccelerationStructure = 0x400000,
+			ShadingRateSource = 0x1000000,
+			GenericRead = (((((0x1 | 0x2) | 0x40) | 0x80) | 0x200) | 0x800),
+			Present = 0,
+			Predication = 0x200,
+			VideoDecodeRead = 0x10000,
+			VideoDecodeWrite = 0x20000,
+			VideoProcessRead = 0x40000,
+			VideoProcessWrite = 0x80000,
+			VideoEncodeRead = 0x200000,
+			VideoEncodeWrite = 0x800000
 		};
 
 		[StructLayout(LayoutKind.Sequential, Pack = 1)]
