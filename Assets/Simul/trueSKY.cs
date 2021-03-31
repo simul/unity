59c2a5894d767556221641c73ff95aa7794cb5a4
Andrew Richards <andyjrichards93@gmail.com>
2021-03-31 09:41:21 +0100

    Updated IL2CPP and function importer for Spectrum.

--- Assets/Simul/trueSKY.cs
+++ Assets/Simul/trueSKY.cs
@@ -3691,6 +3691,10 @@ namespace simul
 		bool _UsingIL2CPP = false;
 		public bool UsingIL2CPP
 		{
+#if UNITY_GAMECORE
+			get { _UsingIL2CPP = true;  return _UsingIL2CPP; }
+			set { _UsingIL2CPP = true; }
+#else
 			get
 			{
 				return _UsingIL2CPP;
@@ -3706,6 +3710,7 @@ namespace simul
 						UnityEngine.Debug.Log(exc.ToString());
 					}
 			}
+#endif
 		}
 
 		//Returns true sizes do not match.
