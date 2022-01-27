c88be5fadf35b6ac9fcd644e598a705719323240
AndrewRichards-Code <andyjrichards93@gmail.com>
2022-01-27 14:52:35 +0000

    trueSKYEditor minor UI message update. UsingIL2CPP is true for all consoles. blitbuf in TrueSkyCamera is disabled.

--- Assets/Simul/Editor/trueSKYEditor.cs
+++ Assets/Simul/Editor/trueSKYEditor.cs
@@ -871,7 +871,7 @@ namespace simul
 						"This allows a single project to have both standard and HDRP scenes.", GUILayout.Height(60.0f));
 					
 					trueSky.UsingIL2CPP = EditorGUILayout.Toggle("Use IL2CPP", trueSky.UsingIL2CPP);
-					EditorGUILayout.LabelField("Default enabled and locked for projects building with GameCore");
+					EditorGUILayout.LabelField("Default enabled and locked for projects building for Xbox or Playstation consoles.");
 				}
 			}
 			// trueSKY Advanced Mode
