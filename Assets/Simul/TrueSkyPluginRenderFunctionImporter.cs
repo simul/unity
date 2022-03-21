57c83bf21fce4fd52dc77355a691b632fe6d148f
Roderick Kennedy <roderick.kennedy@gmail.com>
2022-03-21 12:09:59 +0000

    Don't duplicate function importer.

--- Assets/Simul/TrueSkyPluginRenderFunctionImporter.cs
+++ Assets/Simul/TrueSkyPluginRenderFunctionImporter.cs
@@ -18,9 +18,9 @@ namespace simul
 
 		#else
 			#if UNITY_PS4
-				private const string renderer_dll = @"TrueSkyPluginRenderd";
+				private const string renderer_dll = @"TrueSkyPluginRender";
 			#elif UNITY_PS5
-				private const string renderer_dll = @"TrueSkyPluginRenderd";
+				private const string renderer_dll = @"TrueSkyPluginRender";
 			#elif UNITY_XBOXONE || UNITY_GAMECORE
 				private const string renderer_dll = @"TrueSkyPluginRender_MD";
 			#elif UNITY_IPHONE || UNITY_SWITCH
